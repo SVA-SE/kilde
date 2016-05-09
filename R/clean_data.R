e5001f5e3d66c419693eb03f328792a7986260bb
Thomas Rosendal <thomas.rosendal@sva.se>
2016-05-09 16:29:56 +0200

    Working model using openbugs

--- R/clean_data.R
+++ R/clean_data.R
@@ -5,7 +5,7 @@
 ##' @return list
 ##' @author Thomas Rosendal
 ##' @export
-clean_data <- function(df, country){
+clean_data <- function(df = sample_data(), country){
     df <- df[df$country == country,]
     ASP <- table(df$group, df$ASP)
     GLN <- table(df$group, df$GLN)
@@ -14,7 +14,7 @@ clean_data <- function(df, country){
     PGM <- table(df$group, df$PGM)
     TKT <- table(df$group, df$TKT)
     UNC <- table(df$group, df$UNC)
-    ns <- length(unique(df$group))
+    ns <- length(unique(df$group[df$group != 0]))
     beta <- rep(1,ns)
     nat <- c(length(unique(df$ASP)),
              length(unique(df$GLN)),
@@ -23,15 +23,44 @@ clean_data <- function(df, country){
              length(unique(df$PGM)),
              length(unique(df$TKT)),
              length(unique(df$UNC)))
-    result <- list(ASP,
-                   GLN,
-                   GLT,
-                   GLY,
-                   PGM,
-                   TKT,
-                   UNC,
-                   ns,
-                   beta,
-                   nat)
+    alpha <- structure(.Data=rep(1,ns*ns),.Dim=c(ns,ns))
+    result <- list(ASP = ASP, GLN = GLN, GLT = GLT, GLY = GLY,
+                   PGM = PGM, TKT = TKT, UNC = UNC, ns =  ns,
+                   beta = beta, nat = nat, alpha = alpha)
+## The follwing are arbitrary changes to fulfill the structure of the
+## BUGS code
+    humansASP <- as.vector(result$ASP[1,])
+    humansGLN <- as.vector(result$GLN[1,])
+    humansGLT <- as.vector(result$GLT[1,])
+    humansGLY <- as.vector(result$GLY[1,])
+    humansPGM <- as.vector(result$PGM[1,])
+    humansTKT <- as.vector(result$TKT[1,])
+    humansUNC <- as.vector(result$UNC[1,])
+    sourcesASP <- as.matrix(result$ASP[2:(ns+1),])
+    sourcesGLN <- as.matrix(result$GLN[2:(ns+1),])
+    sourcesGLT <- as.matrix(result$GLT[2:(ns+1),])
+    sourcesGLY <- as.matrix(result$GLY[2:(ns+1),])
+    sourcesPGM <- as.matrix(result$PGM[2:(ns+1),])
+    sourcesTKT <- as.matrix(result$TKT[2:(ns+1),])
+    sourcesUNC <- as.matrix(result$UNC[2:(ns+1),])
+    result <- list(humansASP = humansASP,
+                   humansGLN = humansGLN,
+                   humansGLT = humansGLT,
+                   humansGLY = humansGLY,
+                   humansPGM = humansPGM,
+                   humansTKT = humansTKT,
+                   humansUNC = humansUNC,
+                   sourcesASP = sourcesASP,
+                   sourcesGLN = sourcesGLN,
+                   sourcesGLT = sourcesGLT,
+                   sourcesGLY = sourcesGLY,
+                   sourcesPGM = sourcesPGM,
+                   sourcesTKT = sourcesTKT,
+                   sourcesUNC = sourcesUNC,
+                   ns = ns,
+                   nat = nat,
+                   beta = beta,
+                   alpha = alpha)
+    
     return(result)
 }
---
+++ R/model.R
@@ -0,0 +1,28 @@
+##' A method to run a model in bugs
+##' @title run_model 
+##' @param model The name of the model
+##' This is the file name in the inst/models directory of the package
+##' @param data A dataset
+##' @param inits An inits function
+##' @param parameters Parameters to monitor
+##' @param n.chains The number of chains
+##' @param n.burnin The burning length
+##' @param n.iter The number of iterations
+##' @return A model object
+##' @author Thomas Rosendal
+##' @export
+run_model <- function(model = "SA_allelemodel1.jag",
+                      data,
+                      inits,
+                      parameters,
+                      n.chains = 1,
+                      n.burnin = 99,
+                      n.iter = 999) {
+    bugs(data,
+         inits,
+         parameters,
+         system.file(package = "kilde", paste0("models/", model)),
+         n.chains = n.chains,
+         n.burnin = n.burnin,
+         n.iter = n.iter)
+}
