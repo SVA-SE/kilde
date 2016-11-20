c00db4a287effe77fca47629ed4b1f54ccab6a4b
Thomas Rosendal <thomas.rosendal@sva.se>
2016-11-20 16:52:53 +0100

    Moved the functions into the package
    All datacleaning and model initialization functions for the ST model
    moved into the package. the plot_history function also is complete for
    both ST models in R and BUGS.

--- inst/jukka_r/ST_model/SA_ST_final.R
+++ inst/jukka_r/ST_model/SA_ST_final.R
@@ -1,45 +1,24 @@
 library(kilde)
 DATA <- read.table("../NMDD2015data_vers21_modif.txt",header=TRUE)
 DATA <- DATA[DATA$Country == "FI",]
-
-MCMC <- 1000   ## number of MCMC iterations
-burnin <- 101  ## set the burn-in period length
-FULL <- 0      ## choose 1 for full Bayesian model (semi-supervised), but 
-
-source("dataformattingST.R")
+## First the ST model in R
 ob <- dataformatting_ST(DATA = DATA, UM = 2)
-
-source("initializemcmcST.R")
 result <- initialize_mcmc_ST(ob$ns, ob$STu, MCMC = 100, ob$Nisolates)
-
-source("runmcmcST.R")
 mcmc_ob <- runmcmc_ST(result = result,
            ob = ob,
            h = 0,
            FULL = 0)
-##debugonce(kilde:::plot_history.kilde_rmcmc)
 plot_history(mcmc_ob, 50)
-
-source("plottingST.R")
-
-############################
-#### try BUGS models:
-source("initializebugsST.R")
+## Then the same model in BUGS
 initial_result <- initialize_bugs_ST(ob)
 result_bugs <- kilde::run_bugs(result = initial_result,
                                ob = ob,
-                               MCMC = 500,
+                               MCMC = 1000,
                                n.burnin = 100,
                                FULL = 0,
                                model = "SA_ST_model.jag",
                                n.chains = 1)
 plot_history(result_bugs, 100)
 
-resST <- bugs(data,inits,parameters,"SA_ST_isolate.txt",n.chains=1,n.burnin=burnin,n.iter=MCMC)
-attach.bugs(resST)
-
-MCMC <- MCMC-burnin
-burnin <- 1
-source("plottingST.R")
 
 
